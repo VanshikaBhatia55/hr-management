5c2f379d7af46763904d68c900e2910a22feca0b
himaanshii <himaaanshiii30@gmail.com>
2025-06-06 14:10:12 +0530

    [Himanshi] add. Adding updated entities and corresponding dtos

--- src/main/java/com/hr_management/hr_management/model/dto/JobDTO.java
+++ src/main/java/com/hr_management/hr_management/model/dto/JobDTO.java
@@ -10,6 +10,7 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 public class JobDTO {
+    
     private String jobId;
     private String jobTitle;
     private BigDecimal minSalary;
