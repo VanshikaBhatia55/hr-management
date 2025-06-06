232b59f26b2c67b2cec719ebedf67fb52caf71d6
Raunak kumar sharma <168724821+Raunak1255@users.noreply.github.com>
2025-06-06 14:02:53 +0530

    [Raunak] feat(region): add Region entity and crossponding DTO git add .

---
+++ src/main/java/com/hr_management/hr_management/model/entity/Regions.java
@@ -0,0 +1,27 @@
+package com.hr_management.hr_management.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+@Table(name = "regions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Regions {
+    @Id
+    @Column(name = "region_id", nullable = false)
+    private BigDecimal regionId;
+
+    @Column(name = "region_name", length = 25)
+    private String regionName;
+
+    @OneToMany(mappedBy = "region")
+    private List<Country> countries;
+}
+
