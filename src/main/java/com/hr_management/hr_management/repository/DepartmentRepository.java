56c5dfa548d524af2cdd47e83d056146d2cc97f5
vanshika <vanshika2260@chitkara.edu.in>
2025-06-08 16:30:12 +0530

    [Vanshika] feat(department): add department by ID endpoint and tests

--- src/main/java/com/hr_management/hr_management/repository/DepartmentRepository.java
+++ src/main/java/com/hr_management/hr_management/repository/DepartmentRepository.java
@@ -1,9 +1,15 @@
 package com.hr_management.hr_management.repository;
 import com.hr_management.hr_management.model.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
+import java.util.List;
 
+@Repository
 public interface DepartmentRepository extends JpaRepository<Department, BigDecimal> {
+    List<Department> findByLocationLocationId(BigDecimal locationId);
+
+    List<Department> findByManagerEmployeeId(BigDecimal managerId);
 
 }
