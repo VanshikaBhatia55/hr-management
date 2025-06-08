874864e82bf940b5e00d839a3e9296f31ce1356b
vanshika <vanshika2260@chitkara.edu.in>
2025-06-08 18:53:02 +0530

    [Vanshika] feat(department) : add department by location ID endpoint and test

--- src/main/java/com/hr_management/hr_management/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hr_management/hr_management/exception/GlobalExceptionHandler.java
@@ -9,10 +9,11 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import java.time.LocalDateTime;
 
+
 @ControllerAdvice
 public class GlobalExceptionHandler   {
 
-    @ExceptionHandler
+    @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ApiResponseDto> resourceNotFoundException(ResourceNotFoundException ex, HttpServletRequest request) {
 
         ApiResponseDto err = ApiResponseDto.builder()
@@ -26,11 +27,15 @@ public class GlobalExceptionHandler   {
     }
 
 
-
-
-
-
-
-
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponseDto> globalExceptionHandler(Exception ex, HttpServletRequest request) {
+        ApiResponseDto err = ApiResponseDto.builder()
+                .message("An internal server error occurred: " + ex.getMessage())
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .path(request.getRequestURI())
+                .timestamp(LocalDateTime.now())
+                .build();
+        return new ResponseEntity<>(err, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
 }
