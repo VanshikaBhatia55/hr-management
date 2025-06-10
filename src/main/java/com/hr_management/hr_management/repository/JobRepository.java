d64640133388ac5a51bd4eb130f5219660ba931a
himaanshii <himaaanshiii30@gmail.com>
2025-06-10 11:42:15 +0530

    [Himanshi] add. Adding Http method and test cases for job controller using Mockito

--- src/main/java/com/hr_management/hr_management/repository/JobRepository.java
+++ src/main/java/com/hr_management/hr_management/repository/JobRepository.java
@@ -6,11 +6,16 @@ import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface JobRepository extends JpaRepository<Job, String> {
 
     List<Job> findByMinSalaryLessThanEqualAndMaxSalaryGreaterThanEqual(BigDecimal max, BigDecimal min);
+
+    Optional<Job> findByJobTitle(String jobTitle);
+
+
 
 
 
